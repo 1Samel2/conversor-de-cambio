9e2ca2dab557bf3c7d803f5aae2ec3824e0c2a55
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-01-06 15:29:25 -0300

    saved

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const button = document.getElementById('button-active')
 
 const dolar = 5.2
 
-const converterValues = () =>{
-const inputReais = document.getElementById('input-real').value
-console.log(inputReais / dolar)
+const converterValues = () => {
+    const inputReais = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+    const currencyValueText = document.getElementById('currency-value-text')
+   
 
-}
+    realValueText.innerHTML = new Intl.NumberFormat("pt-BR",{
+        style:"currency",
+        currency:"BRL",
+    }).format(inputReais)
 
-button.addEventListener('click', converterValues)
+    currencyValueText.innerHTML = new Intl.NumberFormat("en-US",{
+        style:'currency',
+        currency:"USD",
+    }).format(inputReais / dolar);
+};
+
+
+button.addEventListener('click', converterValues);
