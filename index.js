a113425106c7ebf06ae4e74649cb3fcf7af74be2
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-01-13 17:36:01 -0300

    avanço

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const dolar = 5.2
 
 const euro = 6
 
+const bitcoin = 0.0000043
+
 
 const converterValues = () => {
     const inputReais = document.getElementById('input-real').value
@@ -18,37 +20,47 @@ const converterValues = () => {
         currency: "BRL",
     }).format(inputReais)
 
-if(select.value === "US$ Dólar americano"){
-    currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-    }).format(inputReais / dolar)}
-
-
-if(select.value === "€ Euro"){
-currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
-    style: "currency",
-    currency: "EUR",
-}).format(inputReais / euro)
+    if (select.value === "US$ Dólar Americano") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputReais / dolar)
     }
-}
+
+    if (select.value === "€ Euro") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputReais / euro)
+    }
+
+    if(select.value === "₿ Bitcoin"){
+        currencyValueText.innerHTML = "0" 
+    currencyValueText.innerHTML = inputReais / bitcoin
+}}
 
 
 const changeCurrency = () => {
     const currencyName = document.getElementById("currency-name")
     const currencyImg = document.getElementById("currency-img")
 
-    if (select.value === "US$ Dólar americano") {
-        currencyName.innerHTML = "Dólar americano"
+    if (select.value === 'US$ Dólar Americano') {
+        currencyName.innerHTML = "Dólar Americano"
         currencyImg.src = "./ASSETS/estados-unidos.svg"
     }
 
-    if (select.value === "€ Euro"){
+    if (select.value === "€ Euro") {
         currencyName.innerHTML = "Euro"
         currencyImg.src = "./ASSETS/Euro.svg"
-}
 
-converterValues()
+    }
+    if (select.value === "₿ Bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./ASSETS/Btcoin.png"
+    }
+
+
+    converterValues()
 
 }
 
